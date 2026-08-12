571757220662967099a16b764b758eaf422d6e0a
ETi0n <ysl04030@gmail.com>
2026-08-12 21:44:27 +0900

    init: 프로젝트 세팅

---
+++ GoalKeeper/GoalKeeper/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GoalKeeper
+//
+//  Created by ETiOn on 8/12/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GoalKeeper/GoalKeeperTests/GoalKeeperTests.swift
@@ -0,0 +1,19 @@
+//
+//  GoalKeeperTests.swift
+//  GoalKeeperTests
+//
+//  Created by ETiOn on 8/12/26.
+//
+
+import Testing
+@testable import GoalKeeper
+
+struct GoalKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
